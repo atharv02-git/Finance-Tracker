696453ae5c2145b763bcc32c17a0a8fb1d1522d0
Atharv <atharvbhandare2001@gmail.com>
2021-12-16 15:32:39 +0530

    adding a cleanup function for the unMount component

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useLogout = () => {
+  // so now we are not updating any state in a component if that component unmounts,if we go to a different page as we've set setIsCancelled to be true so it won't update any state as far as it is true
+  const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState("");
   const [isPending, setIsPending] = useState("");
   const { dispatch } = useAuthContext();
@@ -15,14 +17,20 @@ export const useLogout = () => {
       await projectAuth.signOut();
       // dispatching logout and no need to pass payload as user is going to be null
       dispatch({ type: "LOGOUT" });
-
-      setIsPending(false);
-      setError(null);
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
     } catch (err) {
-      console.error(err);
-      setError(err);
-      setIsPending(false);
+      if (!isCancelled) {
+        console.error(err);
+        setError(err);
+        setIsPending(false);
+      }
     }
   };
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
   return { logout, error, isPending };
 };
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
+  // so now we are not updating any state in a component if that component unmounts,if we go to a different page as we've set setIsCancelled to be true so it won't update any state as far as it is true
+  const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState("");
   const [isPending, setIsPending] = useState("");
   const { dispatch } = useAuthContext();
@@ -28,14 +30,21 @@ export const useSignup = () => {
       // dispatch login action
       dispatch({ type: "LOGIN", payload: res.user });
 
-      setIsPending(false);
-      setError(null);
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
     } catch (err) {
-      console.log(err.message);
-      setError(err.message);
-      setIsPending(false);
+      if (!isCancelled) {
+        console.error(err);
+        setError(err);
+        setIsPending(false);
+      }
     }
   };
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
 
   return { error, isPending, signup };
 };
