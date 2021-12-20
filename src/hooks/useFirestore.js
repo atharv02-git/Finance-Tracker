15981fc370dc0942dfe3084559633d85a2a5765c
Atharv <atharvbhandare2001@gmail.com>
2021-12-20 16:18:44 +0530

    adding firestore documents

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,6 +10,22 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
+    case "IS_PENDING":
+      return { isPending: true, document: null, success: false, error: null };
+    case "ADDED_DOCUMENT":
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
@@ -23,8 +39,24 @@ export const useFirestore = (collection) => {
   //   collection reference
   const ref = projectFirestore.collection(collection);
 
+  // only dispath if not cancelled, i.e we have to trigger response only of user is on the same page as useFirestore hook
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
   // add a document
-  const adddocument = async (doc) => {};
+  const adddocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const addedDocument = await ref.add(doc);
+      dispatchIfNotCancelled({ type: "ADDED_DOCUMENT", payload: addedDocument });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message });
+    }
+  };
 
   // delete document
   const deleteDocument = async (id) => {};
@@ -34,5 +66,5 @@ export const useFirestore = (collection) => {
     return () => setIsCancelled(true);
   }, []);
 
-  return { adddocument, deleteDocument, ref}
+  return { adddocument, deleteDocument, response };
 };
